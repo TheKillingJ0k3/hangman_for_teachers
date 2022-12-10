ad26ec48251a789d0e0f003c6307c5d40bc2b4dc
Th3KillingJ0k3 <ggpatoulis@hotmail.com>
2022-12-10 22:02:36 +0200

    start_game function complete

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ from tkinter import ttk
 from tkinter import messagebox
 import openpyxl
 from openpyxl.styles import Font, Border, Side
+from random import randint
 import os, shutil
 
 
 #GLOBAL VARIABLES
 # var = ''
 # last_name_initial_var = ''
-level_selected = ''
 number_of_letters = ''
 attempts = ''
+english_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+greek_aphabet = ['α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 'χ', 'ψ', 'ω']
+levels_to_columns = {'A1':'1',
+                    'A2':'2',
+                    'B1':'3',
+                    'B2':'4',
+                    'C1':'5',
+                    'C2':'6'
+                    }
 
 #####################################################
-# TODO: gaming section: player chooses level, words of this level are saved in a list, program chooses randomly a word from this list, game begins
-# TODO: exclude articles from the above
-# TODO: languages?
+# TODO: play game function: display word on GUI + hangman - player chooses a letter, if correct, adds it to the word, if not burns an attempt 
+# TODO: one tab for each language
 
 ##################################  FUNCTIONS  ##################################################
 
@@ -48,17 +56,17 @@ def open_excel(excel_file_path):
         wb = openpyxl.Workbook()
         ws = wb.active
         ws['A1'] = 'A1' # or cell.value = skata
-        ws.column_dimensions['A'].width = 22.43
+        ws.column_dimensions['A'].width = 25
         ws['B1'] =  'A2'
-        ws.column_dimensions['B'].width = 22.43
+        ws.column_dimensions['B'].width = 25
         ws['C1'] =  'B1'
-        ws.column_dimensions['C'].width = 22.43
+        ws.column_dimensions['C'].width = 25
         ws['D1'] =  'B2'
-        ws.column_dimensions['D'].width = 22.43
+        ws.column_dimensions['D'].width = 25
         ws['E1'] =  'C1'
-        ws.column_dimensions['E'].width = 22.43
+        ws.column_dimensions['E'].width = 25
         ws['F1'] =  'C2'
-        ws.column_dimensions['F'].width = 22.43
+        ws.column_dimensions['F'].width = 25
         excel_styles(ws)
     wb.save(excel_file_path)
     return wb, ws
@@ -67,21 +75,49 @@ def open_excel(excel_file_path):
 def show_words():
     os.startfile(r".\\Hangman Excel\\Hangman Excel.xlsx") # , data_only=True in case file has a lot of formulas
 
-def select_level(): #connect selection in GUI with a variable
+
+def start_game(level):
+    word_selected = None
+    while word_selected == None:
+        word_selected = ws.cell(row=(randint(2, ws.max_row)), column=int(levels_to_columns[level])).value
+    print(word_selected)
+    hidden_word = word_selected.split()[1]
+    hidden_word = word_selected.split()[0] + ' ' + word_selected.split()[1][0] + (len(hidden_word)-2)*'_ ' + word_selected.split()[1][-1]
+    print(hidden_word)
+    return word_selected, hidden_word
+
+
+def play_game():
     pass
+
+
+def start_level_A1():
+    level_selected = 'A1'
+    start_game(level_selected)
+
+def start_level_A2():
+    level_selected = 'A2'
+    start_game(level_selected)
+
+def start_level_B1():
+    level_selected = 'B1'
+    start_game(level_selected)
+
+def start_level_B2():
+    level_selected = 'B2'
+    start_game(level_selected)
+
+def start_level_C1():
+    level_selected = 'C1'
+    start_game(level_selected)
+
+def start_level_C2():
+    level_selected = 'C2'
+    start_game(level_selected)
+
 
 def function():
     pass
-
-def function1():
-    pass
-
-# def save_variable(): #saves var when input button is pressed
-#     global last_name_initial_var
-#     last_name_initial_var = last_name_entry_initial.get()
-#     print (last_name_initial_var)
-# # as an example, this function is used in last_name_initial_submit
-# # when user writes last name and presses submit, script prints var's value
 
 
 #############################################################################################
@@ -118,26 +154,20 @@ data_menu = Menu(menubar, tearoff=False) #first_lineises new submenu
 action_menu = Menu(menubar, tearoff=False) #first_lineises new submenu
 menubar.add_cascade(label='New Game', menu=action_menu) #creates name of new submenu
 level_menu = Menu(action_menu, tearoff=False)
-level_menu.add_command(label='A1', command=function) #adds option to submenu
-level_menu.add_command(label='A2', command=function) #adds option to submenu
-level_menu.add_command(label='B1', command=function) #adds option to submenu
-level_menu.add_command(label='B2', command=function) #adds option to submenu
-level_menu.add_command(label='C1', command=function) #adds option to submenu
-level_menu.add_command(label='C2', command=function) #adds option to submenu
+level_menu.add_command(label='A1', command=start_level_A1) #adds option to submenu
+level_menu.add_command(label='A2', command=start_level_A2) #adds option to submenu
+level_menu.add_command(label='B1', command=start_level_B1) #adds option to submenu
+level_menu.add_command(label='B2', command=start_level_B2) #adds option to submenu
+level_menu.add_command(label='C1', command=start_level_C1) #adds option to submenu
+level_menu.add_command(label='C2', command=start_level_C2) #adds option to submenu
 action_menu.add_cascade(label='Choose Level', menu=level_menu) #creates name of new submenu
 
 settings_menu = Menu(menubar, tearoff=False)
 menubar.add_cascade(label='Settings', menu=settings_menu) #creates name of new submenu
 settings_menu.add_command(label='Show Saved Words', command=show_words) #  main update_itineraries
-word_settings_menu = Menu(settings_menu, tearoff=False)
-settings_menu.add_cascade(label='Add Words to a Level', menu=word_settings_menu) #creates name of new submenu
-word_settings_menu.add_command(label='A1', command=function) #adds option to submenu
-word_settings_menu.add_command(label='A2', command=function) #adds option to submenu
-word_settings_menu.add_command(label='B1', command=function) #adds option to submenu
-word_settings_menu.add_command(label='B2', command=function) #adds option to submenu
-word_settings_menu.add_command(label='C1', command=function) #adds option to submenu
-word_settings_menu.add_command(label='C2', command=function) #adds option to submenu
-
+settings_menu.add_command(label='Add Words', command=show_words) #creates name of new submenu
+# word_settings_menu = Menu(settings_menu, tearoff=False)
+# word_settings_menu.add_command(label='A1', command=show_words) #adds option to submenu
 
 advice_menu = Menu(menubar, tearoff=False) #first_lineises new submenu
 menubar.add_cascade(label='Advice', menu=advice_menu) #creates name of new submenu
